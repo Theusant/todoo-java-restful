a8c004989311cb5a07bc64087eca28917848eb10
Matheus Santos <matheu.asssant@gmail.com>
2024-01-30 22:14:22 -0300

    model creation

--- src/main/java/com/theusan/toodosimple/models/Task.java
+++ src/main/java/com/theusan/toodosimple/models/Task.java
@@ -1,4 +1,75 @@
 package com.theusan.toodosimple.models;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = Task.TABLE_NAME)
 public class Task {
+
+    public static final String TABLE_NAME = "task";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false, updatable = false)
+    private User user;
+
+    @Column(name = "description", length = 255, nullable = false)
+    @NotNull
+    @NotEmpty
+    @Size(min = 1, max = 255)
+    private String description;
+
+    public Task(){}
+
+    public Task(Long id, User user, String description) {
+        this.id = id;
+        this.user = user;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return Objects.equals(id, task.id) && Objects.equals(user, task.user) && Objects.equals(description, task.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, user, description);
+    }
 }
--- src/main/java/com/theusan/toodosimple/models/User.java
+++ src/main/java/com/theusan/toodosimple/models/User.java
@@ -1,11 +1,14 @@
 package com.theusan.toodosimple.models;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 
@@ -26,11 +29,12 @@ public class User {
 
 
     @Column(name = "username", length = 100, nullable = false, unique = true)
-    @NotNull(groups = {CreateUser.class, UpdateUser.class})
-    @NotEmpty(groups = {CreateUser.class, UpdateUser.class})
-    @Size(groups = {CreateUser.class, UpdateUser.class}, min = 2, max = 100)
+    @NotNull(groups = CreateUser.class)
+    @NotEmpty(groups = CreateUser.class)
+    @Size(groups = CreateUser.class, min = 2, max = 100)
     private String username;
 
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @Column(name = "password", length = 60, nullable = false)
     @NotNull(groups = {CreateUser.class, UpdateUser.class})
     @NotEmpty(groups = {CreateUser.class, UpdateUser.class})
@@ -38,6 +42,10 @@ public class User {
     private String password;
 
     public User(){}
+
+    @OneToMany
+    private List<Task> tasks = new ArrayList<Task>();
+
     public User(Long id, String username, String password) {
         this.id = id;
         this.username = username;
