6300e340bede6dd8951128f643243c62013b24db
Matheus Santos <matheu.asssant@gmail.com>
2024-02-04 22:44:34 -0300

    user-services-creation

---
+++ src/main/java/com/theusan/toodosimple/services/UserService.java
@@ -0,0 +1,50 @@
+package com.theusan.toodosimple.services;
+
+import com.theusan.toodosimple.models.User;
+import com.theusan.toodosimple.repositories.TaskRepository;
+import com.theusan.toodosimple.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public User findById(Long id) {
+        Optional<User> user = this.userRepository.findById(id);
+        return user.orElseThrow( () -> new RuntimeException(
+                "User Not Found !" + id + ", Type: " + User.class.getName()));
+    }
+
+    @Transactional
+    public User create(User obj) {
+        obj.setId(null);
+        obj = this.userRepository.save(obj);
+        this.taskRepository.saveAll(obj.getTasks());
+        return obj;
+    }
+
+    @Transactional
+    public User update(User obj) {
+        User newObj = findById(obj.getId());
+        newObj.setPassword(obj.getPassword());
+        return this.userRepository.save(newObj);
+    }
+
+    public void delete(Long id) {
+        findById(id);
+        try {
+            this.userRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new RuntimeException("Not possible to delete");
+        }
+    }
+}
